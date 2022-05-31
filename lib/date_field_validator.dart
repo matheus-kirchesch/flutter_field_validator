44d43ebba35cb6ff5dfb4a05446259d307ef4798
Kirchesch <KircheschM@hotmail.com>
2022-05-31 16:32:15 -0300

    Date Validator added

---
+++ lib/date_field_validator.dart
@@ -0,0 +1,35 @@
+import 'package:field_validator/field_validator_core.dart';
+
+/// Validates a date field
+class DateFieldValidator extends FieldValidatorCore {
+  const DateFieldValidator({
+    required String errorMessage,
+    this.minDate,
+    this.maxDate,
+  }) : super(errorMessage: errorMessage);
+
+  /// Min acceptable date
+  final DateTime? minDate;
+
+  /// Max acceptable date
+  final DateTime? maxDate;
+
+  @override
+  bool isValid(String? field) {
+    if (field == null) return false;
+
+    DateTime? fieldDate = DateTime.tryParse(field);
+
+    if (fieldDate == null) return false;
+
+    if (minDate != null && fieldDate.isBefore(minDate!)) {
+      return false;
+    }
+
+    if (maxDate != null && fieldDate.isAfter(maxDate!)) {
+      return false;
+    }
+
+    return true;
+  }
+}
