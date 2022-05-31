856166309960ea9a7b26dfcb931257db38db2300
Kirchesch <KircheschM@hotmail.com>
2022-05-31 13:13:20 -0300

    Length Field Validator added

---
+++ lib/length_field_validator.dart
@@ -0,0 +1,31 @@
+import 'package:field_validator/field_validator_core.dart';
+
+/// Validates if the field if not empty
+class LengthFieldValidator extends FieldValidatorCore {
+  const LengthFieldValidator({
+    required String errorMessage,
+    required this.minLength,
+    required this.maxLength,
+  }) : super(errorMessage: errorMessage);
+
+  /// The min length if set
+  final int? minLength;
+
+  /// The max length if set
+  final int? maxLength;
+
+  @override
+  bool isValid(String? field) {
+    // Check the min length
+    if (minLength != null && field != null && field.length < minLength!) {
+      return false;
+    }
+
+    // Check the max length
+    if (maxLength != null && field != null && field.length > maxLength!) {
+      return false;
+    }
+
+    return true;
+  }
+}
