0fae0fc9a34cb33290f3f2f37080060e027e2315
Kirchesch <KircheschM@hotmail.com>
2022-08-20 19:11:01 -0300

    Changed the Field Validator behaviour
    We don't store the field anymore, the validate method accepts a string as the field now, so we can re-use the same validator if we want

--- lib/field_validator.dart
+++ lib/field_validator.dart
@@ -1,32 +1,27 @@
 import 'package:field_validator/field_validator_core.dart';
 
-/// Takes in Field Validators and validates them
+/// Takes in Field Validators and validates them.
 class FieldValidator {
   const FieldValidator(
     this._validators, {
-    String? field,
     bool displayMultipleMessages = false,
-  })  : _field = field,
-        _displayMultipleMessages = displayMultipleMessages;
+  }) : _displayMultipleMessages = displayMultipleMessages;
 
-  /// The field we will validate
-  final String? _field;
-
-  /// All our validators
+  /// All our validators.
   final List<FieldValidatorCore> _validators;
 
-  /// If true will display multiple messages otherwise just the latest
+  /// If true will display multiple messages otherwise just the latest.
   final bool _displayMultipleMessages;
 
-  /// Validates our field validators
-  String? validate() {
+  /// Validates our field validators.
+  String? validate(String field) {
     String? message;
 
     Iterable<FieldValidatorCore> validatorList =
         _displayMultipleMessages ? _validators : _validators.reversed;
 
     for (FieldValidatorCore validator in validatorList) {
-      if (!validator.isValid(_field)) {
+      if (!validator.isValid(field)) {
         if (_displayMultipleMessages && message != null && message.isNotEmpty) {
           if (!message.contains(validator.errorMessage)) {
             message += "\n";
